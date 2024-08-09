941ce6c3e9175a685d1b3fdf3c9c11e35a2d9cc3
mzeeshanzafar28 <mzeeshanzafar28@gmail.com>
2024-08-09 12:49:01 +0500

    Initial Commit of 2 Projects

--- axon-whatsmate-react/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/{path?}', function($path = null){         return View::make('welcome');     })->where('path', '.*'); 
